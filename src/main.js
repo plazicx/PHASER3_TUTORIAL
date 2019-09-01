3207bd9da015de9d20a0cafe2a13576edf87ab7b
Predrag Lazic <plazicx@gmail.com>
2019-08-31 21:21:16 -0500

    adding assets

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ console.log(
 import {LoadScene} from "./scenes/LoadScene";
 import {MenuScene} from "./scenes/MenuScene";
 let game = new Phaser.Game({
-    width: 100,
-    height: 100,
+    width: 800,
+    height: 600,
     scene:[
         /* LoadScene, MenuScene */
         LoadScene
