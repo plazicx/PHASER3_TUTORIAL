8d07b8049ce8b0fa294c208ecfc65227820b762d
Predrag Lazic <plazicx@gmail.com>
2019-08-31 21:34:46 -0500

    loading bar

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -12,6 +12,40 @@ export class LoadScene extends Phaser.Scene{
     }
 
     preload(){
+        this.load.image("title_bg","./assets/title_bg.jpg");
+
+        this.load.image("options_button","./assets/options_button.png");
+
+        this.load.image("play_button","./assets/play_button.png");
+
+        this.load.image("logo","./assets/logo.png");
+
+        this.load.spritesheet("cat","./assets/cat.png", {
+            frameHeight: 32,
+            frameWidth: 32
+        });
+
+        this.load.audio("title_music", "./assets/shuinvy-childhood.mp3");
+
+        //create loading bar - think of add - as creates!
+
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff //white
+            }
+        })
+        /*
+        Loader Events: (loader emmits events)
+            complete - when done loading everything
+            progress - loader number progress in decimal
+        */
+
+        this.load.on("progress",(percent)=>{
+            loadingBar.fillRect(0, this.game.renderer.height / 2., this.game.renderer.width * percent, 50);
+            console.log(percent)
+        })
+
+
 
     }
 
